e6649fc467d26f1df7f2792200848f505ca32ba9
YJ <yjkimjunior@gmail.com>
2018-09-29 21:12:20 +0100

    unit tests for telemetry state update logic

--- packages/frontend/test/Connection.spec.ts
+++ packages/frontend/test/Connection.spec.ts
@@ -4,11 +4,13 @@ const { shallow, mount } = Enzyme;
 import * as sinon from 'sinon';
 import { Server } from 'mock-socket';
 
-import { Types, FeedMessage } from '../../common';
+import { Types, FeedMessage, timestamp, VERSION } from '../../common';
 
 import { Node, Update, State } from '../src/state';
 import { Connection } from '../src/Connection';
 import { PersistentObject, PersistentSet } from '../src/persist';
+
+const { Actions } = FeedMessage;
 
 describe('Connection.ts', () => {
   const fakeWebSocketURL = 'ws://localhost:8080';
@@ -17,11 +19,11 @@ describe('Connection.ts', () => {
   mockServer.on('connection', (socket: any) => {
     console.log('Got connection!');
 
-    socket.on('message', (message: any) => {
-      console.log(message);
-    });
-
-    socket.send('[0,15,8,["BBQ Birch",6],8,["Krumme Lanke",57]]');
+    // socket.on('message', (message: any) => {
+    //   console.log('message received by web socket -> ', message);
+    // });
+    //
+    // socket.send('[0,15,8,["BBQ Birch",6],8,["Pistachios",57]]');
   });
 
   let state: State;
@@ -62,7 +64,7 @@ describe('Connection.ts', () => {
     const pins: PersistentSet<Types.NodeName> = new PersistentSet<Types.NodeName>('key', ((p) => {}));
 
     update = jest.fn((changes) => {
-      console.log(changes);
+      // console.log('message passed to the update function -> ', changes);
 
       state = Object.assign({}, state, changes);
 
@@ -81,77 +83,271 @@ describe('Connection.ts', () => {
 
   afterEach(() => {
     // clear stubs and fakes after each test case
-    // sinon.restore();
     // connection.handleMessages.restore();
   })
 
-  test.only('handle Feed Version message state update', async () => {
+  test('handle Feed Version message state update', async () => {
     connection.handleMessages([
       {
         action: 0,
-        payload: 15
-      }, {
-        action: 8,
-        payload: ["BBQ Birch", 6],
-      }, {
-        action: 8,
-        payload: ["Krumme Lanke", 57]
+        payload: VERSION
       }
     ] as any as FeedMessage.Message[])
 
     expect(update).toHaveBeenCalled();
-
-    console.log(state);
+    expect(state.status).toBe('online');
   })
-
 
   test('handle Best Block message state update', () => {
+    const time = timestamp();
 
+    connection.handleMessages([
+      {
+        action: 1,
+        payload: [1, time, 0],
+      }, {
+        action: 1,
+        payload: [1, time, 123456789]
+      }
+    ] as any as FeedMessage.Message[]);
+
+    expect(update).toHaveBeenCalled();
+    expect(state.blockTimestamp).toBe(time);
+    expect(state.blockAverage).toBe(123456789);
   })
 
-  test('handle Added Node message state update', () => {
+  describe('Add and Remove a Node', () => {
+    const time = timestamp();
 
+    test('handle Added Node message state update', () => {
+      /*
+        Added Node Message (NodeId, NodeDetails, NodeStats, BlockDetails, Maybe<NodeLocation>)
+      */
+      connection.handleMessages([
+        {
+          action: 2,
+          payload: [1, ['Sample Node', 'Sampling', '1.2.3', '0x123456789012345'], [12, 84, 38, 78], [1, 'aoaiuhsf9o2ih389r', 777, time, 829]],
+        }
+      ] as any as FeedMessage.Message[]);
+
+      expect(update).toHaveBeenCalled();
+      expect(state.status).toBe('online');
+      expect(state.nodes).toBeDefined();
+
+      const nodes = [];
+
+      for (const node of state.nodes.values()) {
+        nodes.push(node);
+      }
+
+      const firstNode = nodes[0];
+
+      expect(firstNode.id).toBe(1);
+      expect(firstNode.name).toBe('Sample Node');
+      expect(firstNode.implementation).toBe('Sampling');
+      expect(firstNode.validator).toBe('0x123456789012345');
+      expect(firstNode.peers).toBe(12);
+      expect(firstNode.txs).toBe(84);
+      expect(firstNode.mem).toBe(38);
+      expect(firstNode.cpu).toBe(78);
+      expect(firstNode.height).toBe(1);
+      expect(firstNode.hash).toBe('aoaiuhsf9o2ih389r');
+      expect(firstNode.blockTime).toBe(777);
+      expect(firstNode.blockTimestamp).toBe(time);
+      expect(firstNode.propagationTime).toBe(829);
+    })
+
+    test('handle Located Node message state update', () => {
+
+      /*
+        Located Node
+        [NodeId, Latitude, Longitude, City]
+      */
+
+      connection.handleMessages([
+        {
+          action: 0x04,
+          payload: [1, 30.828, 101.4111, 'Kuala Lumpur']
+        }
+      ] as any as FeedMessage.Message[]);
+
+      expect(update).toHaveBeenCalled();
+
+      const nodes = [];
+
+      for (const node of state.nodes.values()) {
+        nodes.push(node);
+      }
+
+      const firstNode = nodes[0];
+
+      expect(firstNode.lat).toEqual(30.828)
+      expect(firstNode.lon).toEqual(101.4111)
+      expect(firstNode.city).toEqual('Kuala Lumpur')
+    });
+
+    test('handle Time Sync message state update', () => {
+      connection.handleMessages([
+        {
+          action: 0x07,
+          payload: time + 12345432
+        }
+      ] as any as FeedMessage.Message[]);
+
+      const nodes = [];
+
+      for (const node of state.nodes.values()) {
+        nodes.push(node);
+      }
+
+      const firstNode = nodes[0];
+
+      expect(firstNode.blockTimestamp).toBe(time);
+    })
+
+    test('handle Imported Block message state update', () => {
+      /*
+        ImportedBlockMessage [NodeId, BlockDetails]
+        BlockDetails = [BlockNumber, BlockHash, Milliseconds, Timestamp, Maybe<PropagationTime>]
+      */
+      connection.handleMessages([
+        {
+          action: 0x05,
+          payload: ["BBQ Birch", [1, 'ABCDEFGH12345678', 123, time, 48292010]],
+        }
+      ] as any as FeedMessage.Message[]);
+
+      expect(state.sortedNodes).toBeDefined();
+
+      const sortedNodes = [];
+
+      for (const node of state.sortedNodes) {
+        sortedNodes.push(node);
+      }
+
+      const firstSortedNode = sortedNodes[0];
+
+      expect(firstSortedNode.pinned).toBeFalsy();
+      expect(firstSortedNode).toMatchObject({
+          pinned: false,
+          id: 1,
+          name: 'Sample Node',
+          implementation: 'Sampling',
+          version: '1.2.3',
+          validator: '0x123456789012345',
+          peers: 12,
+          txs: 84,
+          mem: 38,
+          cpu: 78,
+          height: 1,
+          hash: 'aoaiuhsf9o2ih389r',
+          blockTime: 777,
+          blockTimestamp: time,
+          propagationTime: 829,
+          lat: 30.828,
+          lon: 101.4111,
+          city: 'Kuala Lumpur'
+        });
+      });
+
+    test('handle Removed Node message state update', () => {
+      /*
+        payload: 1
+      */
+      connection.handleMessages([
+        {
+          action: 3,
+          payload: 1
+        }
+      ] as any as FeedMessage.Message[]);
+
+      expect(update).toHaveBeenCalled();
+      expect(state.nodes.keys()).toMatchObject({});
+    })
   })
 
+  describe('Add and Remove a Chain', () => {
+    test('handle Added Chain message state update', () => {
+      connection.handleMessages([
+        {
+          action: 8,
+          payload: ["BBQ Birch", 6],
+        }, {
+          action: 8,
+          payload: ["Krumme Lanke", 57]
+        }
+      ] as any as FeedMessage.Message[]);
 
-  test('handle Removed Node message state update', () => {
+      expect(update).toHaveBeenCalled();
 
-  })
+      const chains = [];
 
-  test('handle Located Node message state update', () => {
+      for (const chain of state.chains) {
+        chains.push(chain);
+      }
 
-  })
+      const firstChain = chains[0];
+      const secondChain = chains[1];
+
+      expect(chains).toHaveLength(2);
+      expect(firstChain).toEqual([ 'BBQ Birch', 6 ]);
+      expect(secondChain).toEqual([ 'Krumme Lanke', 57 ]);
+    });
+
+    test('handle Node Stats message state update', () => {
+
+    })
 
 
-  test('handle Imported Block message state update', () => {
+    describe('Subscribe and Unsubscribe to Message', () => {
+      test('handle Subscribed To message state update', () => {
+        connection.handleMessages([
+          {
+            action: 0x0A,
+            payload: 'BBQ Birch'
+          }
+        ] as any as FeedMessage.Message[]);
 
-  })
+        expect(update).toHaveBeenCalled();
+        expect(state.subscribed).toBe('BBQ Birch');
 
-  test('handle Node Stats message state update', () => {
+        connection.handleMessages([
+          {
+            action: 0x0A,
+            payload: 'Krumme Lanke'
+          }
+        ] as any as FeedMessage.Message[]);
 
-  })
+        expect(update).toHaveBeenCalled();
+        expect(state.subscribed).toBe('Krumme Lanke');
+      });
 
-  test('handle Time Sync message state update', () => {
+      test('handle Unsubscribed From message state update', () => {
+        connection.handleMessages([
+          {
+            action: 0x0B,
+            payload: 'Krumme Lanke'
+          }
+        ] as any as FeedMessage.Message[]);
 
-  })
+        expect(update).toHaveBeenCalled();
+        expect(state.subscribed).toBeFalsy();
+      })
+    })
 
-  test('handle Added Chain message state update', () => {
+    test('handle Removed Chain message state update', () => {
+      connection.handleMessages([
+        {
+          action: 9,
+          payload: 'BBQ Birch'
+        }, {
+          action: 9,
+          payload: 'Krumme Lanke'
+        }
+      ] as any as FeedMessage.Message[]);
 
-  })
-
-  test('handle Removed Chain message state update', () => {
-
-  })
-
-  test('handle Subscribed To message state update', () => {
-
-  })
-
-  test('handle Unsubscribed From message state update', () => {
-
-  })
-
-  test('handle message update', () => {
-
+      expect(update).toHaveBeenCalled();
+      expect(state.chains.keys()).toMatchObject({});
+    })
   })
 });
