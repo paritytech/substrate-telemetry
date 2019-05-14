4a48a6eecfd0d22afd233c75d097fdfea6e218ef
Maciej Hirsz <1096222+maciejhirsz@users.noreply.github.com>
2019-05-14 15:19:00 +0200

    fix: Stringify network state on demand (#146)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -62,9 +62,11 @@ http.createServer((request, response) => {
       const nodeList = Array.from(chain.nodeList());
       const nodeId = Number(strNodeId);
       const node = nodeList.filter((node) => node.id == nodeId)[0];
-      if (node && typeof node.networkState === 'string') {
+      if (node && node.networkState) {
+        const { networkState } = node;
+
         response.writeHead(200, {"Content-Type": "application/json"});
-        response.write(node.networkState);
+        response.write(typeof networkState === 'string' ? networkState : JSON.stringify(networkState));
       } else {
         response.writeHead(404, {"Content-Type": "text/plain"});
         response.write("Node has disconnected or has not submitted its network state yet");
