0f4f94cfada32184aad648a0199bcdaf7e270d88
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-10-29 11:38:53 +0100

    Switch Rust backend to port 8000

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -91,7 +91,7 @@ fn main() -> std::io::Result<()> {
             .service(web::resource("/network_state/{chain}/{nid}").route(web::get().to_async(state_route)))
             .service(web::resource("/network_state/{chain}/{nid}/").route(web::get().to_async(state_route)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8000")?
     .start();
 
     sys.run()
--- packages/frontend/src/Connection.ts
+++ packages/frontend/src/Connection.ts
@@ -19,7 +19,7 @@ export class Connection {
 
   private static readonly address = window.location.protocol === 'https:'
                                       ? `wss://${window.location.hostname}/feed/`
-                                      : `ws://127.0.0.1:8080/feed`;
+                                      : `ws://127.0.0.1:8000/feed`;
 
   // private static readonly address = 'wss://telemetry.polkadot.io/feed/';
 
