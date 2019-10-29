c381d871c8d853e813f74c2b9ff687b2b89b44ce
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-10-29 12:57:53 +0100

    Bump frame limit to 10mb to see if it stops nodes from disconnecting

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -39,7 +39,7 @@ fn node_route(
     Ok(res.streaming(ws::WebsocketContext::with_codec(
         NodeConnector::new(aggregator, locator, ip),
         stream,
-        Codec::new().max_size(1024 * 1024), // 1mb frame limit
+        Codec::new().max_size(10 * 1024 * 1024), // 10mb frame limit
     )))
 }
 
