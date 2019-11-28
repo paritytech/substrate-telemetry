c3436c5942f2efab562e90d8a7212e0a1d5e6fb6
Maciej Hirsz <hello@maciej.codes>
2019-11-28 11:23:54 +0100

    feat: Attempt ipinfo.io if ipapi.co returns no results

--- backend/src/util/location.rs
+++ backend/src/util/location.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 use actix::prelude::*;
 use rustc_hash::FxHashMap;
 use parking_lot::RwLock;
+use serde::Deserialize;
 
 use crate::chain::{Chain, LocateNode};
 use crate::types::{NodeId, NodeLocation};
@@ -52,6 +53,34 @@ pub struct LocateRequest {
     pub chain: Addr<Chain>,
 }
 
+#[derive(Deserialize)]
+pub struct IPApiLocate {
+    city: Box<str>,
+    loc: Box<str>,
+}
+
+impl IPApiLocate {
+    fn into_node_location(self) -> Option<NodeLocation> {
+        let IPApiLocate { city, loc } = self;
+
+        let mut loc = loc.split(",").map(|n| n.parse());
+
+        let latitude = loc.next()?.ok()?;
+        let longitude = loc.next()?.ok()?;
+
+        // Guarantee that the iterator has been exhausted
+        if loc.next().is_some() {
+            return None;
+        }
+
+        Some(NodeLocation {
+            latitude,
+            longitude,
+            city,
+        })
+    }
+}
+
 impl Handler<LocateRequest> for Locator {
     type Result = ();
 
@@ -66,11 +95,26 @@ impl Handler<LocateRequest> for Locator {
             return
         }
 
-        let ip_req = format!("https://ipapi.co/{}/json", ip);
-        let mut response = match self.client.post(&ip_req).send() {
-            Ok(response) => response,
+        let location = match self.iplocate_ipapi_co(ip).and_then(|location| match location {
+            Some(location) => Ok(Some(location)),
+            None => self.iplocate_ipinfo_io(ip),
+        }) {
+            Ok(location) => location,
             Err(err) => return debug!("POST error for ip location: {:?}", err),
         };
+
+        self.cache.write().insert(ip, location.clone());
+
+        if let Some(location) = location {
+            chain.do_send(LocateNode { nid, location });
+        }
+    }
+}
+
+impl Locator {
+    fn iplocate_ipapi_co(&self, ip: Ipv4Addr) -> Result<Option<Arc<NodeLocation>>, reqwest::Error> {
+        let ip_req = format!("https://ipapi.co/{}/json", ip);
+        let mut response = self.client.post(&ip_req).send()?;
 
         let location = match response.json::<NodeLocation>() {
             Ok(location) => Some(Arc::new(location)),
@@ -80,10 +124,21 @@ impl Handler<LocateRequest> for Locator {
             }
         };
 
-        self.cache.write().insert(ip, location.clone());
+        Ok(location)
+    }
 
-        if let Some(location) = location {
-            chain.do_send(LocateNode { nid, location });
-        }
+    fn iplocate_ipinfo_io(&self, ip: Ipv4Addr) -> Result<Option<Arc<NodeLocation>>, reqwest::Error> {
+        let ip_req = format!("https://ipinfo.io/{}/json", ip);
+        let mut response = self.client.post(&ip_req).send()?;
+
+        let location = match response.json::<IPApiLocate>() {
+            Ok(location) => location.into_node_location().map(Arc::new),
+            Err(err) => {
+                debug!("JSON error for ip location: {:?}", err);
+                None
+            }
+        };
+
+        Ok(location)
     }
 }
