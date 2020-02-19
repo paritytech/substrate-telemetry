589523f1a6f5230903e792f5ba2462fa3ab5a083
Daniel Maricic <daniel@woss.io>
2020-02-19 11:35:42 +0100

    i hope travis works now 😅

--- scripts/travis/build-all.sh
+++ scripts/travis/build-all.sh
@@ -5,5 +5,5 @@ wd="$(dirname $0)"
 # Make sure the other scripts can use relative paths if they want to
 cd "$wd"
 
-bash ./scripts/build-backend.sh
-bash ./scripts/build-frontend.sh
+bash ./scripts/travis/build-backend.sh
+bash ./scripts/travis/build-frontend.sh
