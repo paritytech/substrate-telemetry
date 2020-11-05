35b827ea002b757a2715c2c719b5056f50bf8505
Maciej Hirsz <hello@maciej.codes>
2020-11-06 00:50:49 +0100

    Separate Header from Chain to avoid vdom diffing

--- frontend/src/components/Chain/Chain.tsx
+++ frontend/src/components/Chain/Chain.tsx
@@ -2,19 +2,10 @@ import * as React from 'react';
 import { Connection } from '../../Connection';
 import { Types, Maybe } from '../../common';
 import { State as AppState, Update as AppUpdate } from '../../state';
-import { formatNumber, secondsWithPrecision, getHashData } from '../../utils';
-import { Tab } from './';
+import { getHashData } from '../../utils';
+import { Header } from './';
 import { Tile, Ago, List, Map, Settings, Consensus } from '../';
 import { Persistent, PersistentObject, PersistentSet } from '../../persist';
-
-import blockIcon from '../../icons/cube.svg';
-import finalizedIcon from '../../icons/cube-alt.svg';
-import blockTimeIcon from '../../icons/history.svg';
-import lastTimeIcon from '../../icons/watch.svg';
-import listIcon from '../../icons/list-alt-regular.svg';
-import worldIcon from '../../icons/location.svg';
-import settingsIcon from '../../icons/settings.svg';
-import consensusIcon from '../../icons/cube-alt.svg';
 
 import './Chain.css';
 
@@ -65,56 +56,14 @@ export class Chain extends React.Component<Chain.Props, Chain.State> {
 
     return (
       <div className="Chain">
-        <div className="Chain-header">
-          <Tile icon={blockIcon} title="Best Block">
-            #{formatNumber(best)}
-          </Tile>
-          <Tile icon={finalizedIcon} title="Finalized Block">
-            #{formatNumber(finalized)}
-          </Tile>
-          <Tile icon={blockTimeIcon} title="Average Time">
-            {blockAverage == null
-              ? '-'
-              : secondsWithPrecision(blockAverage / 1000)}
-          </Tile>
-          <Tile icon={lastTimeIcon} title="Last Block">
-            <Ago when={blockTimestamp} />
-          </Tile>
-          <div className="Chain-tabs">
-            <Tab
-              icon={listIcon}
-              label="List"
-              display="list"
-              tab=""
-              current={currentTab}
-              setDisplay={this.setDisplay}
-            />
-            <Tab
-              icon={worldIcon}
-              label="Map"
-              display="map"
-              tab="map"
-              current={currentTab}
-              setDisplay={this.setDisplay}
-            />
-            <Tab
-              icon={consensusIcon}
-              label="Consensus"
-              display="consensus"
-              tab="consensus"
-              current={currentTab}
-              setDisplay={this.setDisplay}
-            />
-            <Tab
-              icon={settingsIcon}
-              label="Settings"
-              display="settings"
-              tab="settings"
-              current={currentTab}
-              setDisplay={this.setDisplay}
-            />
-          </div>
-        </div>
+        <Header
+          best={best}
+          finalized={finalized}
+          blockAverage={blockAverage}
+          blockTimestamp={blockTimestamp}
+          currentTab={currentTab}
+          setDisplay={this.setDisplay}
+        />
         <div className="Chain-content-container">
           <div className="Chain-content">{this.renderContent()}</div>
         </div>
