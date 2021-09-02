b612a3f05498a0f66cdff5a1851f19c5365b8ce9
James Wilson <james@jsdw.me>
2021-09-02 15:57:41 +0100

    Improve comments now that there's an issue to point to

--- backend/common/src/flume_recv_stream.rs
+++ backend/common/src/flume_recv_stream.rs
@@ -1,15 +1,20 @@
+//! A sort-of drop-in replacement to create a Stream from a flume Receiver, because `flume::Receiver::into_stream()`
+//! leaks memory. See:
+//!
+//! https://github.com/zesterer/flume/issues/88
+//!
+//! Hopefully we won't need to use these for long; the issue will probably be resolved fairly prompty and we can
+//! revert back to using the built-in flume methods.
+//!
 use flume::Receiver;
 use futures::stream::poll_fn;
 use futures::{FutureExt, Stream};
 use std::pin::Pin;
 
-/// A drop-in replacement for `flume::RecvStream` to use until a leak is resolved.
+/// A drop-in replacement which is similar to `flume::RecvStream`.
 pub type FlumeRecvStream<'a, T> = Pin<Box<dyn Stream<Item = T> + Send + 'a>>;
 
-/// This is temporary until `flume`'s `.into_stream()` method no longer leaks.
-/// The workaround here is that instead of holding onto a RecvFut, we create one, poll
-/// it, and then let it be dropped each time (the same as if we used `recv_async` directly).
-/// This is a drop-in replacement for `into_stream` so it'll be easy to switch back.
+/// A drop-in replacement for `flume`'s `Receiver::into_stream()` method.
 pub fn flume_receiver_into_stream<'a, T: Send + 'a>(r: Receiver<T>) -> FlumeRecvStream<'a, T> {
     let stream = poll_fn(move |cx| r.recv_async().poll_unpin(cx).map(|r| r.ok()));
     Box::pin(stream)
