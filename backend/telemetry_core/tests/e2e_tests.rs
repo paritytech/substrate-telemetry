fbdbbb3cdbf1a26b44fd9cf395d1f25fd0059bf9
James Wilson <james@jsdw.me>
2021-09-02 15:23:00 +0100

    subscribe by genesis hash in tests

--- backend/telemetry_core/tests/e2e_tests.rs
+++ backend/telemetry_core/tests/e2e_tests.rs
@@ -295,8 +295,6 @@ async fn e2e_feeds_told_about_chain_rename_and_stay_subscribed() {
         })
     };
 
-    let initial_hash = "0x0000000000000000000000000000000000000000000000000000000000000001";
-
     // Subscribe a chain:
     node_tx
         .send_json_text(node_init_msg(1, "Initial chain name", "Node 1"))
@@ -305,7 +303,7 @@ async fn e2e_feeds_told_about_chain_rename_and_stay_subscribed() {
     // Connect a feed and subscribe to the above chain:
     let (feed_tx, mut feed_rx) = server.get_core().connect_feed().await.unwrap();
     feed_tx
-        .send_command("subscribe", initial_hash)
+        .send_command("subscribe", "0x0000000000000000000000000000000000000000000000000000000000000001")
         .unwrap();
 
     // Feed is told about the chain, and the node on this chain:
@@ -484,7 +482,7 @@ async fn e2e_feed_can_subscribe_and_unsubscribe_from_chain() {
 
     // Subscribe it to a chain
     feed_tx
-        .send_command("subscribe", "Local Testnet 1")
+        .send_command("subscribe", "0x0000000000000000000000000000000000000000000000000000000000000001")
         .unwrap();
 
     let feed_messages = feed_rx.recv_feed_messages().await.unwrap();
@@ -517,7 +515,7 @@ async fn e2e_feed_can_subscribe_and_unsubscribe_from_chain() {
 
     // We can change our subscription:
     feed_tx
-        .send_command("subscribe", "Local Testnet 2")
+        .send_command("subscribe", "0x0000000000000000000000000000000000000000000000000000000000000002")
         .unwrap();
     let feed_messages = feed_rx.recv_feed_messages().await.unwrap();
 
@@ -789,7 +787,7 @@ async fn e2e_max_nodes_per_connection_is_enforced() {
     // (now that the chain "Test Chain" is known about, subscribe to it for update messages.
     // This wasn't needed to receive messages re the above since everybody hears about node
     // count changes)
-    feed_tx.send_command("subscribe", "Test Chain").unwrap();
+    feed_tx.send_command("subscribe", "0x0000000000000000000000000000000000000000000000000000000000000001").unwrap();
     feed_rx.recv_feed_messages().await.unwrap();
 
     // Update about non-ignored IDs should still lead to feed output:
