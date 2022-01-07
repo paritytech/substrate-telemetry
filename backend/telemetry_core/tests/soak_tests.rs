ae9910965c60b94665fde27c15e067df4150735e
James Wilson <james@jsdw.me>
2022-01-07 17:00:12 +0000

    cargo fmt

--- backend/telemetry_core/tests/soak_tests.rs
+++ backend/telemetry_core/tests/soak_tests.rs
@@ -227,22 +227,9 @@ async fn run_soak_test(opts: SoakTestOpts) {
 }
 
 /// Return an iterator of `total` unique chain names.
-fn chain_names(total: usize) -> impl Iterator<Item=String> {
-    static CHAIN_STARTS: [&'static str; 5] = [
-        "Polkadot",
-        "Kusama",
-        "Khala",
-        "Wibble",
-        "Moonbase"
-    ];
-    static CHAIN_ENDS: [&'static str; 6] = [
-        "",
-        " Testnet",
-        " Main",
-        "-Dev",
-        "Alpha",
-        "Beta"
-    ];
+fn chain_names(total: usize) -> impl Iterator<Item = String> {
+    static CHAIN_STARTS: [&'static str; 5] = ["Polkadot", "Kusama", "Khala", "Wibble", "Moonbase"];
+    static CHAIN_ENDS: [&'static str; 6] = ["", " Testnet", " Main", "-Dev", "Alpha", "Beta"];
 
     let mut count = 0;
     let mut s_n = 0;
