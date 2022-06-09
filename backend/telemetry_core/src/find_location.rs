8d553aaa34af1ef55b74d9f5e189496ccf225eb4
i1i1 <vanyarybin1@live.ru>
2022-06-09 17:57:04 +0300

    Use geoip for ipv4 city lookup
    Signed-off-by: i1i1 <vanyarybin1@live.ru>

--- backend/telemetry_core/src/find_location.rs
+++ backend/telemetry_core/src/find_location.rs
@@ -18,13 +18,11 @@ use std::net::Ipv4Addr;
 use std::sync::Arc;
 
 use futures::{Sink, SinkExt};
+use maxminddb::{geoip2::City, Reader as GeoIpReader};
 use parking_lot::RwLock;
 use rustc_hash::FxHashMap;
-use serde::Deserialize;
 
-use anyhow::Context;
 use common::node_types::NodeLocation;
-use tokio::sync::Semaphore;
 
 /// The returned location is optional; it may be None if not found.
 pub type Location = Option<Arc<NodeLocation>>;
@@ -52,28 +50,18 @@ where
     );
 
     // Create a locator with our cache. This is used to obtain locations.
-    let locator = Locator::new(cache);
+    let locator = Arc::new(Locator::new(cache));
 
     // Spawn a loop to handle location requests
     tokio::spawn(async move {
-        // Allow 4 requests at a time. acquiring a token will block while the
-        // number of concurrent location requests is more than this.
-        let semaphore = Arc::new(Semaphore::new(4));
-
         loop {
             while let Ok((id, ip_address)) = rx.recv_async().await {
-                let permit = semaphore.clone().acquire_owned().await.unwrap();
                 let mut response_chan = response_chan.clone();
-                let locator = locator.clone();
+                let locator = Arc::clone(&locator);
 
-                // Once we have acquired our permit, spawn a task to avoid
-                // blocking this loop so that we can handle concurrent requests.
                 tokio::spawn(async move {
-                    let location = locator.locate(ip_address).await;
+                    let location = locator.locate(ip_address);
                     let _ = response_chan.send((id, location)).await;
-
-                    // ensure permit is moved into task by dropping it explicitly:
-                    drop(permit);
                 });
             }
         }
@@ -84,23 +72,26 @@ where
 
 /// This struct can be used to make location requests, given
 /// an IPV4 address.
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct Locator {
-    client: reqwest::Client,
+    city: Arc<maxminddb::Reader<&'static [u8]>>,
     cache: Arc<RwLock<FxHashMap<Ipv4Addr, Arc<NodeLocation>>>>,
 }
 
 impl Locator {
-    pub fn new(cache: FxHashMap<Ipv4Addr, Arc<NodeLocation>>) -> Self {
-        let client = reqwest::Client::new();
+    /// taken from here: https://github.com/P3TERX/GeoLite.mmdb/releases/tag/2022.06.07
+    const CITY_DATA: &'static [u8] = include_bytes!("GeoLite2-City.mmdb");
 
-        Locator {
-            client,
+    pub fn new(cache: FxHashMap<Ipv4Addr, Arc<NodeLocation>>) -> Self {
+        Self {
+            city: GeoIpReader::from_source(Self::CITY_DATA)
+                .map(Arc::new)
+                .expect("City data is always valid"),
             cache: Arc::new(RwLock::new(cache)),
         }
     }
 
-    pub async fn locate(&self, ip: Ipv4Addr) -> Option<Arc<NodeLocation>> {
+    pub fn locate(&self, ip: Ipv4Addr) -> Option<Arc<NodeLocation>> {
         // Return location quickly if it's cached:
         let cached_loc = {
             let cache_reader = self.cache.read();
@@ -110,98 +101,25 @@ impl Locator {
             return cached_loc;
         }
 
-        // Look it up via ipapi.co:
-        let mut location = self.iplocate_ipapi_co(ip).await;
+        let City { city, location, .. } = self.city.lookup(ip.into()).ok()?;
+        let city = city
+            .as_ref()?
+            .names
+            .as_ref()?
+            .get("en")?
+            .to_string()
+            .into_boxed_str();
+        let latitude = location.as_ref()?.latitude? as f32;
+        let longitude = location?.longitude? as f32;
 
-        // If that fails, try looking it up via ipinfo.co instead:
-        if let Err(e) = &location {
-            log::warn!(
-                "Couldn't obtain location information for {} from ipapi.co: {}",
-                ip,
-                e
-            );
-            location = self.iplocate_ipinfo_io(ip).await
-        }
-
-        // If both fail, we've logged the errors and we'll return None.
-        if let Err(e) = &location {
-            log::warn!(
-                "Couldn't obtain location information for {} from ipinfo.co: {}",
-                ip,
-                e
-            );
-        }
-
-        // If we successfully obtained a location, cache it
-        if let Ok(location) = &location {
-            self.cache.write().insert(ip, location.clone());
-        }
-
-        // Discard the error; we've logged information above.
-        location.ok()
-    }
-
-    async fn iplocate_ipapi_co(&self, ip: Ipv4Addr) -> Result<Arc<NodeLocation>, anyhow::Error> {
-        let location = self.query(&format!("https://ipapi.co/{}/json", ip)).await?;
-
-        Ok(Arc::new(location))
-    }
-
-    async fn iplocate_ipinfo_io(&self, ip: Ipv4Addr) -> Result<Arc<NodeLocation>, anyhow::Error> {
-        let location = self
-            .query::<IPApiLocate>(&format!("https://ipinfo.io/{}/json", ip))
-            .await?
-            .into_node_location()
-            .with_context(|| "Could not convert response into node location")?;
-
-        Ok(Arc::new(location))
-    }
-
-    async fn query<T>(&self, url: &str) -> Result<T, anyhow::Error>
-    where
-        for<'de> T: Deserialize<'de>,
-    {
-        let res = self
-            .client
-            .get(url)
-            .send()
-            .await?
-            .bytes()
-            .await
-            .with_context(|| "Failed to obtain response body")?;
-
-        serde_json::from_slice(&res)
-            .with_context(|| format!{"Failed to decode '{}'", std::str::from_utf8(&res).unwrap_or("INVALID_UTF8")})
-    }
-}
-
-/// This is the format returned from ipinfo.co, so we do
-/// a little conversion to get it into the shape we want.
-#[derive(Deserialize, Debug, Clone)]
-struct IPApiLocate {
-    city: Box<str>,
-    loc: Box<str>,
-}
-
-impl IPApiLocate {
-    fn into_node_location(self) -> Option<NodeLocation> {
-        let IPApiLocate { city, loc } = self;
-
-        let mut loc = loc.split(',').map(|n| n.parse());
-
-        let latitude = loc.next()?.ok()?;
-        let longitude = loc.next()?.ok()?;
-
-        // Guarantee that the iterator has been exhausted
-        if loc.next().is_some() {
-            return None;
-        }
-
-        Some(NodeLocation {
+        let location = Arc::new(NodeLocation {
+            city,
             latitude,
             longitude,
-            city,
-        })
+        });
+        self.cache.write().insert(ip, Arc::clone(&location));
+
+        Some(location)
     }
 }
 
@@ -210,28 +128,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ipapi_locate_to_node_location() {
-        let ipapi = IPApiLocate {
-            loc: "12.5,56.25".into(),
-            city: "Foobar".into(),
-        };
-
-        let location = ipapi.into_node_location().unwrap();
-
-        assert_eq!(location.latitude, 12.5);
-        assert_eq!(location.longitude, 56.25);
-        assert_eq!(&*location.city, "Foobar");
+    fn locator_construction() {
+        Locator::new(Default::default());
     }
 
     #[test]
-    fn ipapi_locate_to_node_location_too_many() {
-        let ipapi = IPApiLocate {
-            loc: "12.5,56.25,1.0".into(),
-            city: "Foobar".into(),
-        };
-
-        let location = ipapi.into_node_location();
-
-        assert!(location.is_none());
+    fn locate_random_ip() {
+        let ip = "12.5.56.25".parse().unwrap();
+        let node_location = Locator::new(Default::default()).locate(ip).unwrap();
+        assert_eq!(&*node_location.city, "El Paso");
     }
 }
