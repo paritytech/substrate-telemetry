5c60cfbced72adacde258005a8721d80585458b0
Milos Kriz <82968568+miloskriz@users.noreply.github.com>
2022-07-21 00:42:53 +0000

    Please consider updating this script
    Dockerfile current location for the frontend needs correcting

--- scripts/build-docker-frontend.sh
+++ scripts/build-docker-frontend.sh
@@ -15,7 +15,7 @@ IMAGE=substrate-telemetry-frontend
 DOCKER_USER=${DOCKER_USER:-paritytech}
 echo "Publishing $IMAGE as $DOCKER_USER"
 
-docker build -t $IMAGE -f packages/frontend/Dockerfile .
+docker build -t $IMAGE -f ./Dockerfile .
 docker tag $IMAGE $DOCKER_USER/$IMAGE
 
 if [[ "$PUBLISH" = 'true' ]]; then
